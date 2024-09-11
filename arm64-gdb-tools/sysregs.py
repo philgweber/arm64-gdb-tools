dd0bfc8ade948ad03fd7841ba87fbddc03d0ed40
philgweber <philweber@microsoft.com>
2024-09-11 10:56:08 -0700

    Updated sysregs to include some useful EL1 and EL2 registers

--- arm64-gdb-tools/sysregs.py
+++ arm64-gdb-tools/sysregs.py
@@ -2,11 +2,20 @@ import gdb
 import openocd
 
 # https://developer.arm.com/documentation/ddi0595/2020-12/AArch64-Registers
+#   "REG Name" : (ELx,op0,op1,CRn,CRm,op2)
 sysregs = {
-    "TTBR0_EL1" :   (3,0,2,0,0),
-    "MAIR_EL1"  :   (3,0,10,2,0),
-    "SCTLR_EL1" :   (3,0,1,0,0),
-    "DAIF"      :   (3,3,4,2,1),
+    "CNTPCT_EL0":   (0,3,3,14,0,1),
+    "TTBR0_EL1" :   (1,3,0,2,0,0),
+    "MAIR_EL1"  :   (1,3,0,10,2,0),
+    "SCTLR_EL1" :   (1,3,0,1,0,0),
+    "ESR_EL1"   :   (1,3,0,5,2,0),
+    "ESR_EL2"   :   (2,3,4,5,2,0),
+    "ELR_EL1"   :   (1,3,0,4,0,1),
+    "ELR_EL2"   :   (2,3,4,4,0,1),
+    "FAR_EL1"   :   (1,3,0,6,0,0),
+    "FAR_EL2"   :   (2,3,4,6,0,0),
+#    "ICC_CTLR_EL1" :   (1,3,0,12,12,4),
+    "DAIF"      :   (1,3,3,4,2,1),
 }
 
 
@@ -23,6 +32,14 @@ class Sysregs(gdb.Command):
         self.ocd.disconnect()
 
     def invoke(self, arg, from_tty):
+        # By default assume EL1 if we can't get it
+        current_el = 1
+        # MRS command to read current EL 3,0,4,2,2
+        el = self.ocd._mrs(3,0,4,2,2)
+        current_el = int(el,16) >> 2 
+        print("Current EL: " + str(current_el))
         for reg in sysregs:
-            out = self.ocd._mrs(sysregs[reg][0], sysregs[reg][1], sysregs[reg][2], sysregs[reg][3], sysregs[reg][4])
-            print("{r}\t{v}".format(r = reg,v = out))
+            # Only try to print the register if at acceptable EL
+            if(current_el >= sysregs[reg][0]):
+              out = self.ocd._mrs(sysregs[reg][1], sysregs[reg][2], sysregs[reg][3], sysregs[reg][4], sysregs[reg][5])
+              print("{r}\t{v}".format(r = reg,v = out))
